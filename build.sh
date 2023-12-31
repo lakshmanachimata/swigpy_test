3418929fa5517d0f95752dc19690592fa88b12b5
Lakshmana <lakshman.chimata@gmail.com>
2023-12-31 16:54:25 +0530

    added removal of gen files

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
+rm *.so *.o *.cxx test.py
 swig  -python  -c++   -v test.i
 gcc -fPIC -Wall -Wextra  -c test_wrap.cxx  wrapper.cpp  -DDEBUG -I/usr/include/python3.10
 gcc -shared test_wrap.o wrapper.o -lstdc++  $(python3.10-config --ldflags)  -o _test.so 
